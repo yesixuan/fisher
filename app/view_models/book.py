b1174ef6504407edf03e8ce8f12d3bab6bdb9842
yesixuan <18373209286@163.com>
2018-05-25 09:03:44 +0800

    重构代码，但遗留bug

--- app/view_models/book.py
+++ app/view_models/book.py
@@ -5,6 +5,29 @@ Created by Vic on 2018/5/25 06:36
 
 
 class BookViewModel:
+    def __int__(self, book):
+        self.title = book['title']
+        self.publisher = book['publisher']
+        self.page = book['pages'] or ''
+        self.author = '、'.join(book['author'])  # 将数组用顿号链接
+        self.price = book['price']
+        self.summary = book['summary'] or ''
+        self.image = book['image']
+
+
+class BookCollection:
+    def __int__(self):
+        self.total = 0
+        self.books = []
+        self.keyword = ''
+
+    def fill(self, yushu_book, keyword):
+        self.total = yushu_book.total
+        self.keyword = keyword
+        self.books = [BookViewModel(book) for book in yushu_book.books]
+
+
+class _BookViewModel:
     @classmethod
     def package_single(cls, data, keyword):
         returned = {
--- app/web/book.py
+++ app/web/book.py
@@ -2,6 +2,8 @@
 """
 Created by Vic on 2018/5/20 20:27
 """
+import json
+
 from flask import jsonify, request
 
 from app.forms.book import SearchForm
@@ -9,13 +11,15 @@ from . import web
 
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
-from app.view_models.book import BookViewModel
+from app.view_models.book import BookCollection
 
 
 @web.route('/book/search')
 def search():
     # 实力化 wtforms 对象，还需要调用该对象的 validate 方法才能真正实施调用
     form = SearchForm(request.args)
+    books = BookCollection()
+
     if form.validate():
         """
             通过 wtforms 取得表单数据而不是通过 request.args
@@ -24,14 +28,16 @@ def search():
         q = form.q.data.strip()
         page = form.page.data
         isbn_or_key = is_isbn_or_key(q)
+        yushu_book = YuShuBook()
+
         if isbn_or_key == 'isbn':
-            result = YuShuBook.search_by_isbn(q)
-            # 通过 viewModel 层，对数据进行处理
-            result = BookViewModel.package_single(result, q)
+            yushu_book.search_by_isbn(q)
         else:
-            result = YuShuBook.search_by_keyword(q, page)
-            result = BookViewModel.package_collection(result, q)
+            yushu_book.search_by_keyword(q)
+
         # dict 序列化
-        return jsonify(result)
+        books.fill(yushu_book, q)
+        return json.dumps(books, default=lambda o: o.__dict__)
+        # return jsonify(books)
     else:
         return jsonify(form.errors)
